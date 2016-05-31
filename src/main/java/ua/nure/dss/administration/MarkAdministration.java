c7217e183bd311e49686bfb1d9a5793780aa9f96
Roman Riznyk <Roman_Riznyk@epam.com>
2016-05-31 14:51:23 +0300

    change marks (A,B,C,D,F)

--- src/main/java/ua/nure/dss/administration/MarkAdministration.java
+++ src/main/java/ua/nure/dss/administration/MarkAdministration.java
@@ -2,7 +2,11 @@ package ua.nure.dss.administration;
 
 import org.lightadmin.api.config.AdministrationConfiguration;
 import org.lightadmin.api.config.builder.EntityMetadataConfigurationUnitBuilder;
+import org.lightadmin.api.config.builder.PersistentFieldSetConfigurationUnitBuilder;
 import org.lightadmin.api.config.unit.EntityMetadataConfigurationUnit;
+import org.lightadmin.api.config.unit.FieldSetConfigurationUnit;
+import org.lightadmin.api.config.utils.EnumElement;
+
 import ua.nure.dss.domain.Mark;
 
 public class MarkAdministration extends AdministrationConfiguration<Mark> {
@@ -14,6 +18,19 @@ public class MarkAdministration extends AdministrationConfiguration<Mark> {
                 .singularName("Mark")
                 .pluralName("Marks")
                 .build();
+    }
+
+    public FieldSetConfigurationUnit formView(final PersistentFieldSetConfigurationUnitBuilder fragmentBuilder) {
+        return fragmentBuilder
+                .field("criterion").caption("Criterion")
+                .field("alternative").caption("Alternative")
+                .field("mark").caption("Mark").enumeration(
+                        EnumElement.element(1, "F"),
+                        EnumElement.element(2, "D"),
+                        EnumElement.element(3, "C"),
+                        EnumElement.element(4, "B"),
+                        EnumElement.element(5, "A")
+                ).build();
     }
 
 }
